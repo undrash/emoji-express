8938047d16184dcfda7e0d1d4b3157f63ef458ff
Andrei Gaspar <andrei@ildiesig.com>
2019-09-04 21:33:32 +0300

    - Package

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Emojify your API!
+
+  Slashemoji adds emoji support to your website's URLs.
+
+  <br>
+  <br>
+
+  <p style="text-align: center; display: block; margin: auto;">brought you by</p>
+  
+   <br>
+
+  <a href="https://www.boardme.app/">
+    <img style="display: block; margin: auto;" width="100px"
+      src="https://www.boardme.app/public/img/boardme-blue.svg">
+  </a>
+
---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+module.exports.slashEmoji = (app) => {
+
+    // ☎️
+    app.get( "/%E2%98%8E%EF%B8%8F", (req, res) => { req.url = "/contact"; app.handle( req, res ); } );
+
+    // 📞
+    app.get( "/%F0%9F%93%9E", (req, res) => { req.url = "/contact"; app.handle( req, res ); } );
+
+    // 🤙
+    app.get( "/%F0%9F%A4%99", (req, res) => { req.url = "/contact"; app.handle( req, res ); } );
+
+    // 🙋‍♂️
+    app.get( "/%F0%9F%99%8B%E2%80%8D%E2%99%82%EF%B8%8F", (req, res) => { req.url = "/about"; app.handle( req, res ); } );
+
+    // 🙋‍♀️
+    app.get( "/%F0%9F%99%8B%E2%80%8D%E2%99%80%EF%B8%8F", (req, res) => { req.url = "/about"; app.handle( req, res ); } );
+
+};
