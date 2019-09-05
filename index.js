e8c4d5653a3379bf4141edade8db7d8842593747
Andrei Gaspar <andrei@ildiesig.com>
2019-09-05 16:34:07 +0300

    - Fixed about routes

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ module.exports.defaults = (app) => {
     app.get( "/%F0%9F%A4%99", (req, res) => { req.url = "/contact"; app.handle( req, res ); } );
 
     // 🙋‍♂️
-    app.get( "/%F0%9F%99%8B%E2%80%8D%E2%99%82%EF%B8%8F", (req, res) => { req.url = "/about"; app.handle( req, res ); } );
+    app.get( "/%F0%9F%99%8B%E2%80%8D%E2%99%82", (req, res) => { req.url = "/about"; app.handle( req, res ); } );
 
     // 🙋‍♀️
-    app.get( "/%F0%9F%99%8B%E2%80%8D%E2%99%80%EF%B8%8F", (req, res) => { req.url = "/about"; app.handle( req, res ); } );
+    app.get( "/%F0%9F%99%8B%E2%80%8D%E2%99%80", (req, res) => { req.url = "/about"; app.handle( req, res ); } );
 
     // 💳
     app.get( "/%F0%9F%92%B3", (req, res) => { req.url = "/pricing"; app.handle( req, res ); } );
